930eb5e4f87f0109e0d1ec6e55b4efc565e09928
utqkaba <utqkaba@gmail.com>
2024-03-01 14:17:22 +0300

    rates design update

--- src/components/Rates.tsx
+++ src/components/Rates.tsx
@@ -3,9 +3,17 @@ import "./rates.scss";
 function Rates() {
   return (
     <div className="rate">
-      <div>dolar / tl</div>
+      <div className="rate-unit">
+        <div>Dolar / Tl</div>
+        <div>31,3127</div>
+        <div className="rate-change">
+          <section>%0.36</section>
+          <section>arrow</section>
+        </div>
+      </div>
       <div>euro / tl</div>
       <div>altin / tl</div>
+      <div>bitcoin / dolar</div>
     </div>
   );
 }
